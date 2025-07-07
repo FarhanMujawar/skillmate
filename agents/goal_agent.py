c053ea10ba7b71c6949a2d5c3c9f28c9e5a86f60
FarhanMujawar <farhanmujawar0711@gmail.com>
2025-07-08 01:00:02 +0530

    updated

--- agents/goal_agent.py
+++ agents/goal_agent.py
@@ -1,6 +1,5 @@
-from langchain.schema import RunnableConfig
 from utils.chains import simple_prompt_response
 
-def goal_node(state, config: RunnableConfig = None):
+def goal_node(state, config=None):
     user_goal = state["input"]
     return {"goal": user_goal}
