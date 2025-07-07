6695dc98a13c966b664321ab6f293b2e63eeb736
FarhanMujawar <farhanmujawar0711@gmail.com>
2025-07-07 21:07:28 +0530

    4th commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#for installing local package in my virtual environment
+
+from setuptools import find_packages,setup
+
+setup(
+    name='skillmate',
+    version='0.0.1',
+    author='Farhan Mujawar',
+    author_email='farhanmujawar0711@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
